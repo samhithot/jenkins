08166fbb80a7e423796b831f093647e89f6955ce
apmalla <amulyaprasad.malla@gmail.com>
2018-03-17 01:18:34 +0530

    git merge conflict script

---
+++ merge_conflict.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#!/bin/bash
+echo "Enter the Directory Location:"
+read dir
+mkdir -p $dir 
+echo "Enter the File Name :"
+read file && cd $dir && touch $file
+echo "Enter First code :"
+read input
+echo $input >$dir/$file
+cd $dir
+git add $file
+git commit -m "1st commit for git bash script"
+git push -u amulya master
+git branch branch1
+git checkout  branch1
+echo "Enter second code :"
+read input
+echo $input > $file
+git commit -a -m "first commit on branch"
+git push -u amulya branch1
+git checkout master
+echo "Enter third code :"
+read input
+echo $input > $file
+git commit -a -m "second commit on master"
+git merge script
+echo "This for merge conflict"
