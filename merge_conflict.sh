f0ca5703d866181445cef5eac5801888b4834691
apmalla <amulyaprasad.malla@gmail.com>
2018-03-17 03:53:14 +0530

    changes made to script

--- merge_conflict.sh
+++ merge_conflict.sh
@@ -11,20 +11,15 @@ cd $dir
 git add $file
 git commit -m "1st commit for git bash script"
 git push -u amulya master
-git branch branch1
+git branch branch2
 git commit -m "New branch added"
 git push -u amulya branch1
 git checkout  branch1
-echo "Enter second code :"
-read input
-echo "$input" > $file
+read -p "Enter second code :" in1 in2 in3
+echo "$in1" > $file
+echo "$in2" > $file
+echo "$in3" > $file
 git commit -a -m "first commit on branch"
-git push -u amulya branch1
 git checkout master
-echo "Enter third code :"
-read input
-echo "$input" > $file
-git commit -a -m "second commit on master "
-git push -u amulya branch1
-git merge 
-echo "merge success"
+git merge branch1
+
