c5a6da6bb3dec92bf549e1b1a53be520799e4a79
apmalla <amulyaprasad.malla@gmail.com>
2018-03-17 06:17:17 +0530

    code changes

--- merge_conflict.sh
+++ merge_conflict.sh
@@ -11,17 +11,16 @@ cd $dir
 git add $file
 git commit -m "1st commit for git bash script"
 git push -u amulya master
-git branch branch3
+git branch branch4
 git commit -m "New branch added"
-git push -u amulya branch3
-git checkout  branch3
-read -p "Enter second code :" in1 in2 in3
-echo "$in1" > $file
-echo "$in2" > $file
-echo "$in3" > $file
-git commit -a -m "first commit on branch"
+git push -u amulya branch4
+git checkout  branch4
+echo "Enter Second code here :"
+read input
+echo "$input" >$file
+git commit -a -m "2nd commit on branch"
 git checkout master
-git merge branch3
+git merge branch4
 git mergetool
 git commit -a -m "resolved conflict"
-git push -u amulya branch3
+git push -u amulya branch4
