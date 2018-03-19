48843d78a9e74fb32160375d525815baaf676483
apmalla <amulyaprasad.malla@gmail.com>
2018-03-19 23:45:07 +0530

    git url modified in script

--- clone_script.sh
+++ clone_script.sh
@@ -4,7 +4,7 @@ giturl=https://github.com/apmalla/bootcamp.git
 echo "Enter the Directory Location to clone"
 read dir
 cd $dir 
-git clone -b $git_url && ls -l
+git clone -b $git_url ./  && ls -l
 git remote show bootcamp
 git fetch
 echo "Enter the branch you want to checkout"
